ffa4e1bfd363f26256a6be9a82c55177086f89da
Abhishek Kumar <abhishekhzb8@gmail.com>
2021-04-02 12:03:15 +0530

    created a function

--- app/src/main/java/com/newapp/firstappsai/MainActivity.kt
+++ app/src/main/java/com/newapp/firstappsai/MainActivity.kt
@@ -7,5 +7,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //this is comment
+        view()
+    }
+
+    private fun view() {
+
     }
 }
